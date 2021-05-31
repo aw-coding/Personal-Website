eb4cea569e6d0bbafbf259b8dc0e167fda181fa6
unknown <andrewwood90@hotmail.com>
2021-05-31 23:43:53 +0100

    added MainPage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import { Component } from 'react';
 import './App.css';
+import MainPage from './components/MainPage';
+
+//add material-ui for styling
+// https://material-ui.com/
+
+// add random quote section... where would that component go?
 
 function App() {
   return(
-    <h1>hello world</h1>
+    <MainPage />
   )
 }
 
---
+++ src/components/MainPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+// This is the main page, the top of the component chain
+const MainPage = () => {
+
+  return (
+    <>  
+    <h1>Andrew Wood</h1>
+    <h2>Software Developer</h2>
+    </>
+
+  );
+
+}
+
+export default MainPage;
